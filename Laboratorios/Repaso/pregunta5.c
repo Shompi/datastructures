7d36d0187eedcde0b4eb290f00694564111a58a6
ShompiFlen <shompire02@gmail.com>
2020-03-03 01:35:56 -0300

    Potencia con punteros

---
+++ Laboratorios/Repaso/pregunta5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void potencia(int *a, int *b, int *r)
+{
+  int *i = (int *)malloc(sizeof(int));
+
+  // Cuidado con la última parte del for, no se puede hacer:
+  // *(i)++, *i++, (*i ++).
+
+  for (*i = 0; *i < *b; *i = *i + 1)
+  {
+    printf("%i\n", *i);
+    *r = *r * *a;
+  }
+}
+
+int main()
+{
+  /* 5. Implementar una función que calcule la potencia de un número.
+Ejecute su función en el Main e imprima el resultado obtenido.
+Restricción 1: Para todas las declaraciones de variables que se utilicen dentro de su
+función deberán ser punteros.
+Restricción 2: Utilice el siguiente prototipo de función. (No es posible modificar)
+Observación: En el argumento int *r debe guardar el resultado. */
+
+  int a, b, resultado = 1;
+
+  printf("Ingrese base y exponente: ");
+  scanf("%i %i", &a, &b);
+
+  potencia(&a, &b, &resultado);
+
+  printf("Resultado: %i", resultado);
+}
