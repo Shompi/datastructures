566d6150f79a993990f4ddc3b847efe101ef2359
ShompiFlen <shompire02@gmail.com>
2024-11-15 04:18:38 -0300

    modificado el codigo para solo usar punteros

--- Laboratorios/Repaso/pregunta5.c
+++ Laboratorios/Repaso/pregunta5.c
@@ -3,33 +3,40 @@
 
 void potencia(int *a, int *b, int *r)
 {
-  int *i = (int *)malloc(sizeof(int));
+	int *i = (int *)malloc(sizeof(int));
 
-  // Cuidado con la última parte del for, no se puede hacer:
-  // *(i)++, *i++, (*i ++).
+	// Cuidado con la última parte del for, no se puede hacer:
+	// *(i)++, *i++, (*i ++).
 
-  for (*i = 0; *i < *b; *i = *i + 1)
-  {
-    printf("%i\n", *i);
-    *r = *r * *a;
-  }
+	for (*i = 0; *i < *b; *i = *i + 1)
+	{
+		printf("%i\n", *i);
+		*r = *r * *a;
+	}
 }
 
 int main()
 {
-  /* 5. Implementar una función que calcule la potencia de un número.
+	/* 5. Implementar una función que calcule la potencia de un número.
 Ejecute su función en el Main e imprima el resultado obtenido.
 Restricción 1: Para todas las declaraciones de variables que se utilicen dentro de su
 función deberán ser punteros.
 Restricción 2: Utilice el siguiente prototipo de función. (No es posible modificar)
 Observación: En el argumento int *r debe guardar el resultado. */
 
-  int a, b, resultado = 1;
+	// int a, b, resultado = 1;
+	int *a = NULL, *b = NULL, *resultado = NULL;
 
-  printf("Ingrese base y exponente: ");
-  scanf("%i %i", &a, &b);
+	a = (int *)malloc(sizeof(int));
+	b = (int *)malloc(sizeof(int));
+	resultado = (int *)malloc(sizeof(int));
+	printf("Ingrese base y exponente: ");
+	// scanf("%i %i", &a, &b);
+	scanf("%i %i", a, b);
 
-  potencia(&a, &b, &resultado);
+	// potencia(&a, &b, &resultado);
+	potencia(a, b, resultado);
+	printf("Resultado: %i", resultado);
 
-  printf("Resultado: %i", resultado);
+	return 0;
 }
