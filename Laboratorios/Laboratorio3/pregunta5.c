ee41d4333f883b9329a71c2e2eac426c740d83f2
ShompiFlen <shompire02@gmail.com>
2024-11-15 03:33:29 -0300

    terminada pregunta 5, y añadida liberación de memoria

--- Laboratorios/Laboratorio3/pregunta5.c
+++ Laboratorios/Laboratorio3/pregunta5.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void calcularParImpar(int **matriz, int n, int m, int *par, int *impar)
 {
@@ -21,7 +23,60 @@ void calcularParImpar(int **matriz, int n, int m, int *par, int *impar)
 	}
 }
 
+void imprimir_matriz(int **matriz, int n, int m)
+{
+	for (int i = 0; i < n; i++)
+	{
+		int *fila = *(matriz + i);
+		for (int j = 0; j < m; j++)
+		{
+			printf("[%i]", *(fila + j));
+		}
+		printf("\n");
+	}
+}
+
+void free_memory(int **matriz, int n, int m)
+{
+	for (int i = 0; i < n; i++)
+	{
+		int *fila = *(matriz + i);
+		free(fila);
+		free(matriz + i);
+	}
+}
+
 int main()
 {
-	// Falta completar main
+	srand(time(NULL));
+
+	int n = 10;
+	int m = 10;
+	int **matriz = (int **)malloc(sizeof(int *) * n);
+
+	for (int i = 0; i < m; i++)
+	{
+		*(matriz + i) = (int *)malloc(sizeof(int) * m);
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		int *fila = *(matriz + i);
+		for (int j = 0; j < m; j++)
+		{
+			*(fila + j) = rand() % 5 + 1;
+		}
+	}
+
+	int *pares = (int *)malloc(sizeof(int));
+	int *impares = (int *)malloc(sizeof(int));
+
+	calcularParImpar(matriz, n, m, pares, impares);
+
+	imprimir_matriz(matriz, n, m);
+
+	printf("Numeros pares: %i\n", *(pares));
+	printf("Numeros impares: %i\n", *(impares));
+	free_memory(matriz, n, m);
+	return 0;
 }
